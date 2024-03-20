9b19439e51b881e4804608041931081b09cd60f9
DaniloCarSan <danilocarsan@gmail.com>
2024-03-20 11:02:08 -0300

    centralizar rodapé

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,10 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <hr />
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
+        <center>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
@@ -25,6 +22,11 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
+        <p>
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+        </p>
+        </center>
       </footer>
     )
   }
